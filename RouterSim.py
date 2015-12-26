055af8eb21fd42abeadb9f451190f0a6a3f636fd
WGDEVS <WilliamGaoDEV@gmail.com>
2015-12-25 23:28:31 -0500

    updated code to python 3

--- RouterSim.py
+++ RouterSim.py
@@ -6,7 +6,6 @@ to build and maintain the network by modifying routers, save/load the
 network to external files, display information about the routers in
 the network and, use pathfinding algorithms to find routes within the 
 network.
-
 Made by WGDEV, some rights reserved, see license.txt for more info
 '''
 
@@ -23,28 +22,29 @@ Returns:
 def findPath(Router, Target):
     global Main
     q = [[0,Router,str(Router)]] #q is short for the queue, looks like [[delay,router #,path so far]...]
-    mapy = [q[0]] #Keeps track of the best path to a router so far, sorted based on router #,uses the same format as que
-    
-    q.insert(0,[False,False])
-    
+    mapy = [[0,Router]] #Keeps track of the best path to a router so far, sorted based on router #,uses the same format as q except no path so far
+
     if not binSearch(0,Main,Router)[1]:
         return("Initial router not found!")
-    
+
+    q.insert(0,[False,False])    
+
     while (q[0][1] != Target):
         q.pop(0)
+
         if len(q) <= 0:
             return("No path found!")
         
         ln = binSearch(1,mapy,q[0][1])
         if ln[1] and mapy[ln[0]][0] < q[0][0]:
-            continue
+            continue        
         
         ln = binSearch(0,Main, q[0][1])
         for i in Main[ln[0]][1]:
             ln2 = binSearch(1,mapy,i[0])
             nc = q[0][0] + i[1]
             if not ln2[1]:
-                mapy.insert(ln2[0],[nc,i[0],q[0][2]+"->" +str(i[0])])
+                mapy.insert(ln2[0],[nc,i[0]])
             else:
                 if mapy[ln2[0]][0] > nc:
                     mapy[ln2[0]][0] = nc
@@ -54,7 +54,7 @@ def findPath(Router, Target):
             if ln3[0] <= 0:
                 ln3[0] = 1
             q.insert(ln3[0],[nc,i[0],q[0][2]+"->" +str(i[0])])  
-    
+
     return("Delay is " + str(q[0][0]) + " with a path of " + q[0][2])
 
 '''
@@ -76,7 +76,7 @@ def binSearch(Index,List,Value):
             return [0,False]
         else:
             return [1,False]
-    
+
     mini = 0
     maxi = len(List)-1
     while(maxi-mini>1):
@@ -97,139 +97,139 @@ def binSearch(Index,List,Value):
         return [maxi,False]
     else:
         return [maxi+1,False]
-    
-        
+
+
 '''
-    Description: Prints all the availabe commands
-    '''
+Description: Prints all the availabe commands
+'''
 def showOptions():    
     print("Command -> Effect:")
     print("save [filename] -> saves the network to the file")
     print("load [filename] -> loads the network from the file")
     print("tracert [router 1] [router 2] -> finds the path between two routers")
-    
+
     print("link [router 1] [router 2] [delay] -> adds/updates a link between two routers")
     print("remove [router 1] [router 2] -> removes the link between two routers")
     print("delete [router] -> deletes the router")
     print("neighbour [router] -> lists all routers directly linked to the specified router")
     print("topology -> lists all routers in the network")
-    
+
 '''
-      Description: Prints all the routers in the network
-      '''    
+Description: Prints all the routers in the network
+'''    
 def showRouters():
     print("Showing all routers in the network:")
     for i in Main:
         print("Router " + str(i[0]) + " has " + str(len(i[1])) + " link(s) to other routers")
 
 '''
-        Description: Prints all the routers that are directly linked to a router
-        Parameters:
-        Router1: The specified router's number
-        '''
+Description: Prints all the routers that are directly linked to a router
+Parameters:
+    Router1: The specified router's number
+'''
 def showRoutes(Router1):
     ln = binSearch(0,Main,Router1)
     if (not ln[1]):
         print ("Router does not exist!")
         return
     print("Showing neighbour(s) for router "+str(Router1)+":")
-    
+
     for i in Main[ln[0]][1]:
         print("Other router is " + str(i[0]) + " with a delay of " + str(i[1]) + ".")
-    
-        '''
-          Description: Takes two routers, creates them if they do not exist and, links them or changes the cost if the link already exists
-          Parameters:
-          Router1: The first router's number
-          Router2: The second router's number
-          Cost: The new cost of the link
-          Returns: An string represening the number of routers that were created by the function
-          '''    
+
+'''
+Description: Takes two routers, creates them if they do not exist and, links them or changes the cost if the link already exists
+Parameters:
+    Router1: The first router's number
+    Router2: The second router's number
+    Cost: The new cost of the link
+Returns: A string represening the number of routers that were created by the function
+'''    
 def addRoute(Router1,Router2,Cost):
     if Cost < 1:
         return "Delay must be at least 1!"
     if Router1 == Router2:
         return "Links cannot loop!"
-    
+
     global Main
     outp = 0
-    
+
     ln = binSearch(0,Main,Router1)
     if ln[1] == False:
         Main.insert(ln[0],[Router1,[]])
         outp +=1        
-        
+
     ln2 = binSearch(0,Main[ln[0]][1],Router2)
     if ln2[1] == False:
         Main[ln[0]][1].insert(ln2[0],[Router2,Cost])
     else:
         Main[ln[0]][1][ln2[0]][1] = Cost
-   
+
     ln = binSearch(0,Main,Router2)
     if ln[1] == False:
         Main.insert(ln[0],[Router2,[]])
         outp +=1        
-        
+
     ln2 = binSearch(0,Main[ln[0]][1],Router1)
     if ln2[1] == False:
         Main[ln[0]][1].insert(ln2[0],[Router1,Cost])
     else:
         Main[ln[0]][1][ln2[0]][1] = Cost    
-    
+
     return "Link sucessfully added, with " + str(outp) + " router(s) automatically installed."
 
 '''
-          Description: Takes two routers, deletes the link between them and, deletes them if they do not have any links left afterwards
-          Parameters:
-          Router1: The first router's number
-          Router2: The second router's number
-          Returns: An string represening the number of routers that were deleted by the function
-          '''      
+Description: Takes two routers, deletes the link between them and, deletes them if they do not have any links left afterwards
+Parameters:
+    Router1: The first router's number
+Router2: The second router's number
+Returns: A string represening the number of routers that were deleted by the function
+'''      
 def removeRoute(Router1,Router2):    
     global Main    
     outp = 0
-    
+
     ln1 = binSearch(0,Main,Router1)
     if (not (ln1[1])):
         return "One or more specified router(s) do not exist!"
-    
+
     ln = binSearch(0,Main[ln1[0]][1],Router2)
     if (not ln[1]):
         return "The link does not exist!"
-    
+
     Main[ln1[0]][1].pop(ln[0])
     if len(Main[ln1[0]][1]) <= 0:
         Main.pop(ln1[0])
         outp += 1
-    
+
     ln1 = binSearch(0,Main,Router2)    
     if (not (ln1[1])):
         return "One or more specified router(s) do not exist!"    
-    
+
     ln = binSearch(0,Main[ln1[0]][1],Router1)
     if (not ln[1]):
         return "The link does not exist!"
-    
+
     Main[ln1[0]][1].pop(ln[0])
     if len(Main[ln1[0]][1]) <= 0:
         Main.pop(ln1[0])
         outp += 1    
-    
+
     return "Link sucessfully deleted, with " + str(outp) + " router(s) automatically removed."
 
 '''
-          Description: Deletes a router and any links associated with the router, also automatically deletes any routers with no links
-          Parameters:
-          Router1: The specified router's number
-          Returns: An string represening the number of routers that were deleted by the function
-          '''      
+Description: Deletes a router and any links associated with the router, also automatically deletes any routers with no links
+Parameters:
+    Router1: The specified router's number
+Returns: A string represening the number of routers that were deleted by the function
+'''      
 def deleteRoute(Router1):
     global Main
     outp = 0
     ln = binSearch(0,Main,Router1)
     if (not ln[1]):
         return "Router does not exist!"
-    
+
     while (len(Main[ln[0]][1]) > 0):
         ln1 = binSearch(0,Main,Main[ln[0]][1][0][0])
         ln2 = binSearch(0,Main[ln1[0]][1],Router1)
@@ -242,13 +242,13 @@ def deleteRoute(Router1):
         Main[ln[0]][1].pop(0)
     Main.pop(ln[0])
     return "Router sucessfully deleted, with " + str(outp) + " other router(s) automatically removed."
-        
+
 '''
-          Description: Saves the network to a text document
-          Parameters:
-          Name: The text document's name
-          Returns: An string represening if the network was successfully saved
-          '''              
+Description: Saves the network to a text document
+Parameters:
+    Name: The text document's name
+Returns: A string represening if the network was successfully saved
+'''              
 def save(Name):
     try:
         File = open(Name, "w")
@@ -263,11 +263,11 @@ def save(Name):
         return "Error saving network!"
 
 '''
-              Description: Loads the network from a text document
-              Parameters:
-              Name: The text document's name
-              Returns: An string represening if the network was successfully loaded
-              '''    
+Description: Loads the network from a text document
+Parameters:
+    Name: The text document's name
+Returns: A string represening if the network was successfully loaded
+'''    
 def load(Name):
     try:
         File = open(Name, "r")
@@ -290,11 +290,11 @@ def load(Name):
         return "Network loaded."
     except:
         return "Error loading network!"
-        
+
 #The  actual program starts here
 print("Welcome to RouterSim v1.0 made by WGDEV, some rights reserved see license.txt for more info, enter \"?\" for a list of commands.")
 while (True):
-    i = raw_input().lower().split(' ')
+    i = input().lower().split(' ')
     if (i[0] == "?"):
         showOptions()
     elif (i[0] == "link"):
